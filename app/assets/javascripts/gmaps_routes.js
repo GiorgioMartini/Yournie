e87b4541c53d484189ec72e7c2d29ffcd31556b5
giorgio <jorgeantonio82@googlemail.com>
2014-11-14 13:12:24 +0100

    draw routes in direction mode works

--- app/assets/javascripts/gmaps_routes.js
+++ app/assets/javascripts/gmaps_routes.js
@@ -1,15 +1,33 @@
-var artRouteCoordinates = [
-    new google.maps.LatLng( 52.503143,  13.412260),
-    new google.maps.LatLng( 52.500228,  13.423025),
-    new google.maps.LatLng( 52.501192,  13.423784),
-    new google.maps.LatLng( 52.500767,  13.425243)
-  ];
+// var artRouteCoordinates = [
+//     new google.maps.LatLng( 52.503143,  13.412260),
+//     new google.maps.LatLng( 52.500228,  13.423025),
+//     new google.maps.LatLng( 52.501192,  13.423784),
+//     new google.maps.LatLng( 52.500767,  13.425243)
+//   ];
+/*
 
 
+***********************
+*=Variables
+***********************
+*/
 
 
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+
+/*
+***********************
+*=Initialize
+***********************
+*/
 
 function initialize() {
+		//Create new DirectionsRenderer
+		directionsDisplay = new google.maps.DirectionsRenderer();
+
         var mapOptions = {
           center: { lat:   52.502541, lng:   13.412209},
           zoom: 14,
@@ -17,26 +35,55 @@ function initialize() {
                 
 
         };
-     
+    // Create new Map in the map-canvas ID
     map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
 
-	printAllMarkers();
+	renderRoute();
+
+	// Directions Display
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  directionsDisplay.setMap(map);
+}
+
+/*
+***********************
+*=Functions
+***********************
+*/
+
+function add_marker(myMarker){
+	var marker = new google.maps.Marker({
+	      position: myMarker,
+	      map: map,
+   
+		});		
 }
 
 
-	function draw_route(listOfMarkers) {
-	var routePath = new google.maps.Polyline({
-		    path: listOfMarkers,
-		    geodesic: true,
-		    strokeColor: '#2cd000',
-		    strokeOpacity: 1.0,
-		    strokeWeight: 2
-		  });
-		routePath.setMap(map);
-	}
+function renderRoute() { 
+	url = window.location.href+'.json';
+	$.get(url, function(data){
+		
+		
+		var stopLatLongs = makeStopArray(data.stops);
+
+ 		//draw_route( stopLatLongs );
+ 		drawDirectionRoute( stopLatLongs );
+ 		drawMarkers(stopLatLongs);
+	});
+}
 
 
+
+function drawMarkers(stopData) {
+	console.log(stopData);
+	stopData.forEach(function(stop){	
+			add_marker(stop);
+				
+		})
+		
+}
 
 
 function makeStopArray(stops) {
@@ -49,64 +96,53 @@ function makeStopArray(stops) {
 }
 
 
-	function draw_route(listOfMarkers) {
-	var routePath = new google.maps.Polyline({
-		    path: listOfMarkers,
-		    geodesic: true,
-		    strokeColor: '#2cd000',
-		    strokeOpacity: 1.0,
-		    strokeWeight: 2
-		  });
-		routePath.setMap(map);
+// function draw_route(listOfMarkers) {
+// var routePath = new google.maps.Polyline({
+// 	    path: listOfMarkers,
+// 	    geodesic: true,
+// 	    strokeColor: '#2cd000',
+// 	    strokeOpacity: 1.0,
+// 	    strokeWeight: 2
+// 	  });
+// 	routePath.setMap(map);
+// }
+
+
+function drawDirectionRoute (markers) {
+	var request = {
+	origin: markers[0],
+	destination: markers[markers.length-1], 
+	waypoints: createWaypoints(markers),
+	optimizeWaypoints: true,
+    travelMode: google.maps.TravelMode.WALKING
+	
 	}
+	console.log(request) 
+	directionsService.route(request, function(response, status) {
+	    if (status == google.maps.DirectionsStatus.OK) {
+	      directionsDisplay.setDirections(response);
+	    }
+	})      
+}
 
-
-
-
-
-	function add_marker(myMarker){
-		var marker = new google.maps.Marker({
-		      position: myMarker,
-		      map: map,
-	   
- 		});		
-	}
-
-
-
-
-
-	function printAllMarkers () { 
-		url = window.location.href+'.json';
-		console.log(url);
-		$.get(url, function(data){
-			
-			data.stops.forEach(function(stop){
-				stop.stop_lat
-				stop.stop_long	
-
-
-
-				console.log(stop.stop_lat);
-				console.log(stop.stop_long);
-
-				var latlong = new google.maps.LatLng(stop.stop_lat,stop.stop_long);
-				add_marker(latlong);
-					
-
-
-			})
-			
-			var stopLatLongs = makeStopArray(data.stops);
-
-	 		draw_route( stopLatLongs );
+function createWaypoints(stops){
+	return stops.map(function(stop){
+		var waypoint = {
+			location: stop,
+			stopover:false
+		}
+			return waypoint;
 
 		});
-	}
+}
 
 
+/*
+***********************
+*=Direction Functions
+***********************
+*/
 
-   
 
 google.maps.event.addDomListener(window, 'load', initialize);
 	// var routePath = new google.maps.Polyline({
