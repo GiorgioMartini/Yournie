6f02584caf5b822d02a330c125c14268f7ba0b2a
giorgio <jorgeantonio82@googlemail.com>
2014-12-12 18:32:54 +0100

    fixed thumnails

--- app/uploaders/route_image_uploader.rb
+++ app/uploaders/route_image_uploader.rb
@@ -41,7 +41,7 @@ class RouteImageUploader < CarrierWave::Uploader::Base
   # end
 
    version :thumb do
-    process :resize_to_limit => [800, 800]
+    process :resize_to_limit => [400, 400]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
